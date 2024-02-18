098647fa4f3929eccbc80c94b24bfba523aa6342
PinguDash <132189736+PinguDash@users.noreply.github.com>
2024-02-18 21:31:46 +0100

    erste ver
    naja

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        id id = new id();
+
+        System.out.println(id.strafIDs());
+
+    }
+
+    private int[] vorstrafenregister = new int[25];
+
+
+
+    public String mord(){
+        return "w";
+    }
+
+
+}
---
+++ id.java
@@ -0,0 +1,22 @@
+public class id {
+    public String strafIDs(){
+        String text = """
+                0 - Mord
+                1 - Haustiermord
+                2 - Nutztiermord
+                3 - Diebstahl zweiten Grades (bsp. grundlegende Ressourcen wie Granit, kurzfristiger Schaden
+                4 - Diebstahl ersten Grades (langfristiger Schaden)
+                5 - Totschlag
+                6 - Vorsätzlicher Schweinemord
+                7 - Diebstahl
+                8 - Toxizität/sonstiges bzgl. Chat
+                9 - Sachbeschädigung zweiten Grades
+                10 - Sachbeschädigung ersten Grades
+                """;
+        return text;
+    }
+
+    public String strafe(int id){
+
+    }
+}
